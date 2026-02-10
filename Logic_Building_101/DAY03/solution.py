ecec41b1f4931a2213cf09806ee8fac8f8bdc064
Damodhar Vanjarapu <76522052+damodhar786@users.noreply.github.com>
2026-02-10 21:32:28 +0530

    commit day3-p3

--- Logic_Building_101/DAY03/solution.py
+++ Logic_Building_101/DAY03/solution.py
@@ -40,3 +40,23 @@ print(f"Area of Triangle: {area_of_triangle}")
 print("-" * 63)
 
 #---------------------------------------------------------------------
+# 3. **Write a Program to Find the Quotient and Remainder of Two Integers:**
+
+#    Write a program where the user enters two integers (divisor and dividend) and calculates their quotient and remainder. For example:
+
+#    - **Input:** **`Dividend: 22, Divisor: 7`**
+#    - **Output:**
+
+#    ```markdown
+#    Quotient: 3
+#    Remainder: 1
+#    ```
+
+integer1 = int(input("Enter 1st number: "))
+integer2 = int(input("Enter 2nd number: "))
+
+quotient = int(integer1 / integer2)
+remainder = integer1 % integer2
+
+print(f"Quotient: {quotient}")
+print(f"Remainder: {remainder}")
