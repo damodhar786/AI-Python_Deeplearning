d2aa058d6464dd7057b278019c18cc39f8106c96
Damodhar Vanjarapu <76522052+damodhar786@users.noreply.github.com>
2026-02-10 21:15:33 +0530

    Commit- Day3-P1

---
+++ Logic_Building_101/DAY03/solution.py
@@ -0,0 +1,16 @@
+# # DAY - 03
+
+# 1. **Program to Calculate the Square and Cube of a Number:**
+
+#    Write a program where the user enters a number, and the program calculates its square and cube. For example:
+
+#    - **Input:** **`Enter a number: 3`**
+#    - **Output:** **`Square: 9, Cube: 27`**
+
+number = int(input("Enter a number: "))
+square = number ** 2
+cube = number ** 3
+print(f"Square: {square}, Cube: {cube}")
+print("-" * 63)
+
+#---------------------------------------------------------
