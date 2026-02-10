fbdf2b4884d8a030ee514d3a35aa72ca9d9f1a8f
Damodhar Vanjarapu <76522052+damodhar786@users.noreply.github.com>
2026-02-10 21:23:15 +0530

    Commit Day3-P2

--- Logic_Building_101/DAY03/solution.py
+++ Logic_Building_101/DAY03/solution.py
@@ -14,3 +14,29 @@ print(f"Square: {square}, Cube: {cube}")
 print("-" * 63)
 
 #---------------------------------------------------------
+
+# 2. **Program to Calculate the Area of a Circle and Triangle:**
+
+#    Write a program to calculate the area of a circle given its radius and a triangle given its base and height. For example:
+
+#    - **Input:** **`Radius: 5, Base: 10, Height: 4`**
+#    - **Output:**
+
+#    ```markdown
+#    Area of Circle: 78.5
+#    Area of Triangle: 20
+#    ```
+
+radius = int(input("Enter Radius of Circle: "))
+base = int(input("Enter Base of a Triangle: "))
+height = int(input("Enter Height of a Triangle: "))
+
+area_of_circle = 3.14 * (radius ** 2)
+area_of_triangle = 0.5 * base * height
+
+print(f"Area of Circle: {area_of_circle}")
+print(f"Area of Triangle: {area_of_triangle}")
+
+print("-" * 63)
+
+#---------------------------------------------------------------------
